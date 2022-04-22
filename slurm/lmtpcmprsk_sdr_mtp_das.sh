712593256349d8b6c8008e92be67d87a50632f15
Diego Armando Salazar Barreto <dsalazarb@javeriana.edu.co>
2022-04-21 20:14:39 -0500

    ...

--- slurm/lmtpcmprsk_sdr_mtp_das.sh
+++ slurm/lmtpcmprsk_sdr_mtp_das.sh
@@ -11,15 +11,15 @@ echo "$SLURM_ARRAY_TASK_ID"
 
 source ~/.bashrc
 
-spack load /wvljhbh
+spack load /wvljhbh # R
 ## module load openblas/0.2.20 gcc/6.3.0 r/4.0.3 r-packages/default
 ## k7ewedg openblas@0.3.8~ilp64+pic+shared threads=none
-spack load /k7ewedg
+# spack load /k7ewedg # openblas
 ## t5bjiq6 gcc@8.2.0~binutils~nvptx~piclibs~strip languages=c,c++,fortran
-spack load /t5bjiq6
+# spack load /t5bjiq6 # gcc
 ## module load openblas/0.2.20 gcc/6.3.0 r/4.1.1 r-packages/default
 cd ~/aki_comprisks-master/
 
 R CMD BATCH --no-save --no-restore \
   '--args int_type=mtp est_type=sdr' \
-  R/run_lmtp.R logs/run_lmtp_sdr_mtp_das.Rout
+  R/run_lmtp.R results/run_lmtp_sdr_mtp_das.Rout
