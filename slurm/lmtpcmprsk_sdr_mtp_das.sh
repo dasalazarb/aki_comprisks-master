49f20574e08bbc7c60c2133df743a918c3088f0e
Diego Armando Salazar Barreto <dsalazarb@javeriana.edu.co>
2022-04-26 08:57:03 -0500

    ...

--- slurm/lmtpcmprsk_sdr_mtp_das.sh
+++ slurm/lmtpcmprsk_sdr_mtp_das.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=40G
+#SBATCH --mem=30G
 #SBATCH --array=1-20
 ## Command(s) to run:
 echo "$SLURM_ARRAY_TASK_ID"
@@ -12,12 +12,6 @@ echo "$SLURM_ARRAY_TASK_ID"
 source ~/.bashrc
 
 spack load /wvljhbh # R
-## module load openblas/0.2.20 gcc/6.3.0 r/4.0.3 r-packages/default
-## k7ewedg openblas@0.3.8~ilp64+pic+shared threads=none
-# spack load /k7ewedg # openblas
-## t5bjiq6 gcc@8.2.0~binutils~nvptx~piclibs~strip languages=c,c++,fortran
-# spack load /t5bjiq6 # gcc
-## module load openblas/0.2.20 gcc/6.3.0 r/4.1.1 r-packages/default
 cd ~/aki_comprisks-master/
 
 R CMD BATCH --no-save --no-restore \
