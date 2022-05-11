59063c1894ac9a6058077b403e01e6af1d1143e4
Diego Armando Salazar Barreto <dsalazarb@javeriana.edu.co>
2022-05-11 10:47:21 -0500

    add sdr_static

---
+++ slurm/lmtpcmprsk_sdr_static_das.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=deathAsOutcome
+#SBATCH --partition=panda
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=40G
+#SBATCH --array=1-20
+## Command(s) to run:
+echo "$SLURM_ARRAY_TASK_ID"
+
+source ~/.bashrc
+
+spack load /wvljhbh # R
+cd ~/aki_comprisks-master/
+
+R CMD BATCH --no-save --no-restore \
+  '--args int_type=static est_type=sdr' \
+  R/run_lmtp.R results/run_lmtp_sdr_mtp_das.Rout
